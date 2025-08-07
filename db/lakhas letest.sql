23199e072ce12618aea086a711c1f62e83b6df02
SUDAIS AHMED <130484580+sudaisahmed123@users.noreply.github.com>
2025-08-07 15:00:30 +0500

    update database

--- db/lakhas letest.sql
+++ db/lakhas letest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 04, 2025 at 08:28 PM
+-- Generation Time: Aug 07, 2025 at 09:58 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.2.0
 
